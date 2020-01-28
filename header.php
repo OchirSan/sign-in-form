b92dccaadfe6472a49a66e5dce25a0764f84bced
OchirSan <39770608+OchirSan@users.noreply.github.com>
2020-01-28 18:39:55 +0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link href="styles.css" rel="stylesheet">
+    <title>Project - ведение списков</title>
+    <style>
+        .main-menu.bottom li a{
+            font-size: 12px;
+        }
+        .main-menu li a{
+            font-size: 16px;
+        }
+    </style>    
+</head>
+
+<body>
+
+    <div class="header">
+        <div class="logo"><img src="i/logo.png" width="68" height="23" alt="Project"></div>
+        <div class="clearfix"></div>
+    </div>
+
+    <div class="clearfix">
+        <ul class="main-menu">
+            <li><a href="#">Главная</a></li>
+            <li><a href="#">О нас</a></li>
+            <li><a href="#">Контакты</a></li>
+            <li><a href="#">Новости</a></li>
+            <li><a href="#">Каталог</a></li>
+        </ul>
+    </div>
