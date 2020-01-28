ab7ed9f67c51a1249eb3d5ef3df09a75bd6ccaf7
OchirSan <39770608+OchirSan@users.noreply.github.com>
2020-01-28 18:53:15 +0300

    Create footer.php

---
+++ template/footer.php
@@ -0,0 +1,14 @@
+<div class="clearfix">
+        <ul class="main-menu bottom">
+            <li><a href="#">Главная</a></li>
+            <li><a href="#">О нас</a></li>
+            <li><a href="#">Контакты</a></li>
+            <li><a href="#">Новости</a></li>
+            <li><a href="#">Каталог</a></li>
+        </ul>
+    </div>
+
+    <div class="footer">&copy;&nbsp;<nobr>2018</nobr> Project.</div>
+
+</body>
+</html>
